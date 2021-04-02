90c4b54fbfffdee163d4eeacac5c35c3568c4b6f
kaushik-rishi <rishi.cp01@gmail.com>
2021-04-02 12:41:58 +0530

    Divide the loading of search engine into another file

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 // Some JS {Bloated} for startpage.
 
 var query_url = "https://www.google.com/search?q="
-var searchEngines = {
-    yt: {icon: " ", url: "https://www.youtube.com/results?search_query="},
-    g: {icon: " ", url: "https://www.google.com/search?q="},
-    gt: {icon: " ", url: "https://github.com/search?q="},
-    red: {icon: " ", url: "https://www.reddit.com/search/?q="},
-    dj: {icon: " ", url: "https://docs.djangoproject.com/en/3.1/search/?q="},
-    py: {icon: " ", url: "https://docs.python.org/3/search.html?q="},
-    tw: {icon: " ", url: "https://twitter.com/search?q="},
-    arch: {icon: " ", url: "https://wiki.archlinux.org/index.php?search="},
-    js: {icon: " ", url: "https://developer.mozilla.org/en-US/search?q="},
-}
+
 var search = document.querySelector("#query")
 var logo = document.querySelector("#search_logo")
 var autoComplete = document.querySelector("#auto-complete")
@@ -23,8 +13,8 @@ function engineHtmlGenerator(query, searchEngines) {
     var filteredItems = Object.keys(searchEngines).filter((value) => {
         return value.startsWith(query.slice(1))
     })
-    
-    for (var i=0; i<filteredItems.length; i++) {
+
+    for (var i = 0; i < filteredItems.length; i++) {
         var key = filteredItems[i]
         var restString = (":" + key).replace(query, "")
         engineTemplate += `
@@ -58,7 +48,7 @@ document.addEventListener("keyup", (e) => {
         window.location = query_url
     }
     if (e.code == "Tab") {
-        if (Object.keys(searchEngines).find((value) => value == query.slice(1))){
+        if (Object.keys(searchEngines).find((value) => value == query.slice(1))) {
             logo.innerHTML = searchEngines[query.slice(1)].icon
             query_url = searchEngines[query.slice(1)].url
             search.value = ""
